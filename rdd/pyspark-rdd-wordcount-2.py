232b9726ae01329808094427f5d234123136c8be
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-12 15:48:23 +0200

    Update pyspark-rdd-wordcount-2.py

--- rdd/pyspark-rdd-wordcount-2.py
+++ rdd/pyspark-rdd-wordcount-2.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 14 10:20:19 2020
-"""
-
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 data = ["Project Gutenberg’s",
