fda54fc7de649c2d8b1dd4cdc22dc7d9499414f3
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-12 15:48:44 +0200

    Update pyspark-rdd-wordcount-2.py

--- rdd/pyspark-rdd-wordcount-2.py
+++ rdd/pyspark-rdd-wordcount-2.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 data = ["Project Gutenberg’s",
         "Alice’s Adventures in Wonderland",
@@ -14,18 +15,22 @@ for element in rdd.collect():
 rdd2=rdd.flatMap(lambda x: x.split(" "))
 for element in rdd2.collect():
     print(element)
+        
 #map
 rdd3=rdd2.map(lambda x: (x,1))
 for element in rdd3.collect():
     print(element)
+        
 #reduceByKey
 rdd4=rdd3.reduceByKey(lambda a,b: a+b)
 for element in rdd4.collect():
     print(element)
+        
 #map
 rdd5 = rdd4.map(lambda x: (x[1],x[0])).sortByKey()
 for element in rdd5.collect():
     print(element)
+        
 #filter
 rdd6 = rdd5.filter(lambda x : 'a' in x[1])
 for element in rdd6.collect():
