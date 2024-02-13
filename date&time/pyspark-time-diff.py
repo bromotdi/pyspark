39667d2ec065483ca64175cc68b348b40723b1e1
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-13 22:35:05 +0200

    Update pyspark-time-diff.py

--- date&time/pyspark-time-diff.py
+++ date&time/pyspark-time-diff.py
@@ -1,20 +1,19 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
 # Create SparkSession
 spark = SparkSession.builder \
           .appName('SparkByExamples.com') \
           .getOrCreate()
 
-
-dates = [("1","2019-07-01 12:01:19.111"),
-    ("2","2019-06-24 12:01:19.222"),
-    ("3","2019-11-16 16:44:55.406"),
-    ("4","2019-11-16 16:50:59.406")
+dates = [("1","2023-07-01 12:01:19.111"),
+    ("2","2023-06-24 12:01:19.222"),
+    ("3","2023-11-16 16:44:55.406"),
+    ("4","2023-11-16 16:50:59.406")
     ]
 
 df = spark.createDataFrame(data=dates, schema=["id","from_timestamp"])
 
-from pyspark.sql.functions import *
 df2=df.withColumn('from_timestamp',to_timestamp(col('from_timestamp')))\
   .withColumn('end_timestamp', current_timestamp())\
   .withColumn('DiffInSeconds',col("end_timestamp").cast("long") - col('from_timestamp').cast("long"))
@@ -32,11 +31,11 @@ df2.withColumn('DiffInHours',round(col('DiffInSeconds')/3600))\
   .show(truncate=False)
   
 #Difference between two timestamps when input has just timestamp
-
 data= [("12:01:19.000","13:01:19.000"),
     ("12:01:19.000","12:02:19.000"),
     ("16:44:55.406","17:44:55.406"),
     ("16:50:59.406","16:44:59.406")]
+
 df3 = spark.createDataFrame(data=data, schema=["from_timestamp","to_timestamp"])
 
 df3.withColumn("from_timestamp",to_timestamp(col("from_timestamp"),"HH:mm:ss.SSS")) \
@@ -46,13 +45,11 @@ df3.withColumn("from_timestamp",to_timestamp(col("from_timestamp"),"HH:mm:ss.SSS
    .withColumn("DiffInHours",round(col("DiffInSeconds")/3600)) \
    .show(truncate=False)
    
-#
-
-
 df3 = spark.createDataFrame(
         data=[("1","07-01-2019 12:01:19.406")], 
         schema=["id","input_timestamp"]
-        )
+)
+
 df3.withColumn("input_timestamp",to_timestamp(col("input_timestamp"),"MM-dd-yyyy HH:mm:ss.SSS")) \
     .withColumn("current_timestamp",current_timestamp().alias("current_timestamp")) \
     .withColumn("DiffInSeconds",current_timestamp().cast("long") - col("input_timestamp").cast("long")) \
@@ -62,7 +59,6 @@ df3.withColumn("input_timestamp",to_timestamp(col("input_timestamp"),"MM-dd-yyyy
     .show(truncate=False)
     
 #SQL
-
-spark.sql("select unix_timestamp('2019-07-02 12:01:19') - unix_timestamp('2019-07-01 12:01:19') DiffInSeconds").show()
-spark.sql("select (unix_timestamp('2019-07-02 12:01:19') - unix_timestamp('2019-07-01 12:01:19'))/60 DiffInMinutes").show()
-spark.sql("select (unix_timestamp('2019-07-02 12:01:19') - unix_timestamp('2019-07-01 12:01:19'))/3600 DiffInHours").show()
+spark.sql("select unix_timestamp('2023-07-02 12:01:19') - unix_timestamp('2019-07-01 12:01:19') DiffInSeconds").show()
+spark.sql("select (unix_timestamp('2023-07-02 12:01:19') - unix_timestamp('2019-07-01 12:01:19'))/60 DiffInMinutes").show()
+spark.sql("select (unix_timestamp('2023-07-02 12:01:19') - unix_timestamp('2019-07-01 12:01:19'))/3600 DiffInHours").show()
