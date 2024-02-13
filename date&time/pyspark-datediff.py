a82bfe19600c475f88d261be4b81a42518bfa48e
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-13 22:31:46 +0200

    Update pyspark-datediff.py

--- date&time/pyspark-datediff.py
+++ date&time/pyspark-datediff.py
@@ -1,14 +1,13 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
 # Create SparkSession
 spark = SparkSession.builder \
           .appName('SparkByExamples.com') \
           .getOrCreate()
-data = [("1","2019-07-01"),("2","2019-06-24"),("3","2019-08-24")]
+data = [("1","2023-07-01"),("2","2021-06-24"),("3","2022-08-24")]
 
 df=spark.createDataFrame(data=data,schema=["id","date"])
-
-from pyspark.sql.functions import *
 
 df.select(
       col("date"),
@@ -23,13 +22,11 @@ df.withColumn("datesDiff", datediff(current_date(),col("date"))) \
   .withColumn("yearsDiff_round",round(months_between(current_date(),col("date"))/lit(12),2)) \
   .show()
 
-data2 = [("1","07-01-2019"),("2","06-24-2019"),("3","08-24-2019")]  
+data2 = [("1","07-01-2023"),("2","06-24-2023"),("3","08-24-2023")]  
 df2=spark.createDataFrame(data=data2,schema=["id","date"])
 df2.select(
     to_date(col("date"),"MM-dd-yyyy").alias("date"),
     current_date().alias("endDate")
     )
-
-#SQL
 
 spark.sql("select round(months_between('2019-07-01',current_date())/12,2) as years_diff").show()
