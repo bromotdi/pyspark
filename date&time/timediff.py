c27cb5fbf8da9877b2d4adf0fed2fee5d0a5435e
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-13 22:27:50 +0200

    Update timediff.py

--- date&time/timediff.py
+++ date&time/timediff.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 24 22:42:50 2019
-
-@author: prabha
-"""
-
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, round
@@ -16,13 +9,12 @@ spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 schema = StructType([
             StructField("input_timestamp", StringType(), True)])
 
-dates = ['2019-07-01 12:01:19.111',
-    '2019-06-24 12:01:19.222',
-    '2019-11-16 16:44:55.406',
-    '2019-11-16 16:50:59.406']
+dates = ['2023-07-01 12:01:19.111',
+    '2023-06-24 12:01:19.222',
+    '2023-11-16 16:44:55.406',
+    '2023-11-16 16:50:59.406']
 
 df = spark.createDataFrame(list( zip(dates)), schema=schema)
-
 df.withColumn('input_timestamp',to_timestamp(col('input_timestamp')))\
   .withColumn('current_timestamp', current_timestamp().alias('current_timestamp'))\
   .withColumn('DiffInSeconds',current_timestamp().cast(LongType()) - col('input_timestamp').cast(LongType()))\
