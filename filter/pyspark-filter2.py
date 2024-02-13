81b6841ef48d8e01d4368c7ddb873084b05b2a2d
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-13 22:07:18 +0200

    Update pyspark-filter2.py

--- filter/pyspark-filter2.py
+++ filter/pyspark-filter2.py
@@ -6,6 +6,7 @@ spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 data2 = [(1,"James Smith"), (2,"Michael Rose"),
     (3,"Robert Williams"), (4,"Rames Rose"),(5,"Rames rose")
   ]
+
 df2 = spark.createDataFrame(data = data2, schema = ["id","name"])
 
 df2.filter(df2.name.like("%rose%")).show()
