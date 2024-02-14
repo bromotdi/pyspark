77d737ab182adbb4a4c50fe012c9c6d58ec99c49
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-14 22:02:15 +0200

    Update pyspark-string-timestamp.py

--- string/pyspark-string-timestamp.py
+++ string/pyspark-string-timestamp.py
@@ -1,14 +1,13 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
 # Create SparkSession
 spark = SparkSession.builder \
           .appName('SparkByExamples.com') \
           .getOrCreate()
 
-from pyspark.sql.functions import *
-
 df=spark.createDataFrame(
-        data = [ ("1","2019-06-24 12:01:19.000")],
+        data = [ ("1","2023-06-24 12:01:19.000")],
         schema=["id","input_timestamp"])
 df.printSchema()
 
@@ -21,11 +20,8 @@ df.withColumn('timestamp', \
          to_timestamp('input_timestamp').cast('string')) \
   .show(truncate=False)
   
-
-df.select(to_timestamp(lit('06-24-2019 12:01:19.000'),'MM-dd-yyyy HH:mm:ss.SSSS')) \
+df.select(to_timestamp(lit('06-24-2023 12:01:19.000'),'MM-dd-yyyy HH:mm:ss.SSSS')) \
   .show(truncate=False)
-  
-
 #SQL string to TimestampType
 spark.sql("select to_timestamp('2019-06-24 12:01:19.000') as timestamp")
 #SQL CAST timestamp string to TimestampType
