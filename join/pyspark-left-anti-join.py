76b359c392295582fbaeb85af8614353cf2a2bde
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-15 22:34:19 +0200

    Update pyspark-left-anti-join.py

--- join/pyspark-left-anti-join.py
+++ join/pyspark-left-anti-join.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-author SparkByExamples.com
-"""
-
 import pyspark
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('machinelearninggeeks.com').getOrCreate()
