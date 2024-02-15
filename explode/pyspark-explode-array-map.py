9703a5c776cd99bc1e784adb804ec104c2feb7f3
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-15 22:28:17 +0200

    Update pyspark-explode-array-map.py

--- explode/pyspark-explode-array-map.py
+++ explode/pyspark-explode-array-map.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 24 22:42:50 2019
-
-@author: Naveen
-"""
-
 import pyspark
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('pyspark-by-examples').getOrCreate()
