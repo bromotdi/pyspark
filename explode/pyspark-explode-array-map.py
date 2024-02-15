8cd50931c278fa1e78dc8240bb0d47e7d800b5f6
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-15 22:28:58 +0200

    Update pyspark-explode-array-map.py

--- explode/pyspark-explode-array-map.py
+++ explode/pyspark-explode-array-map.py
@@ -1,5 +1,7 @@
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+
 spark = SparkSession.builder.appName('pyspark-by-examples').getOrCreate()
 
 arrayData = [
@@ -13,30 +15,24 @@ df = spark.createDataFrame(data=arrayData, schema = ['name','knownLanguages','pr
 df.printSchema()
 df.show()
 
-from pyspark.sql.functions import explode
 df2 = df.select(df.name,explode(df.knownLanguages))
 df2.printSchema()
 df2.show()
 
-from pyspark.sql.functions import explode
 df3 = df.select(df.name,explode(df.properties))
 df3.printSchema()
 df3.show()
 
-from pyspark.sql.functions import explode_outer
 """ with array """
 df.select(df.name,explode_outer(df.knownLanguages)).show()
 """ with map """
 df.select(df.name,explode_outer(df.properties)).show()
 
-
-from pyspark.sql.functions import posexplode
 """ with array """
 df.select(df.name,posexplode(df.knownLanguages)).show()
 """ with map """
 df.select(df.name,posexplode(df.properties)).show()
 
-from pyspark.sql.functions import posexplode_outer
 """ with array """
 df.select(df.name,posexplode_outer(df.knownLanguages)).show()
 
