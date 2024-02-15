f2195d7233d6049e5999df72a8b34669f65c67fb
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-15 22:32:43 +0200

    Update pyspark-join.py

--- join/pyspark-join.py
+++ join/pyspark-join.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 14 10:20:19 2020
-
-@author: prabha
-"""
-
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
