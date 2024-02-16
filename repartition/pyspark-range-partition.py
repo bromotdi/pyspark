ce28523bb6d57c8f6535bfe0c62c5053ea1b266b
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-16 21:36:56 +0200

    Update pyspark-range-partition.py

--- repartition/pyspark-range-partition.py
+++ repartition/pyspark-range-partition.py
@@ -1,11 +1,9 @@
 from pyspark.sql import SparkSession
 
-# Create SparkSession
 spark = SparkSession.builder \
           .appName('SparkByExamples.com') \
           .getOrCreate()
           
-
 data = [(1,10),(2,20),(3,10),(4,20),(5,10),
     (6,30),(7,50),(8,50),(9,50),(10,30),
     (11,10),(12,10),(13,40),(14,40),(15,40),
@@ -27,4 +25,3 @@ df.repartitionByRange(3,"value") \
   .write.option("header",True) \
   .mode("overwrite") \
   .csv("c:/tmp/range-partition-count")
-
