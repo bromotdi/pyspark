69128c6fc6694d7d6667c8417f98b758bbcf391c
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-18 21:49:39 +0200

    Update pyspark-sql-case-when.py

--- sql-functions/pyspark-sql-case-when.py
+++ sql-functions/pyspark-sql-case-when.py
@@ -1,4 +1,7 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import when, col
+from pyspark.sql.functions import expr
+
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 data = [("James","M",60000), ("Michael","M",70000),
         ("Robert",None,400000), ("Maria","F",500000),
@@ -9,7 +12,6 @@ df = spark.createDataFrame(data = data, schema = columns)
 df.show()
 
 #Using When otherwise
-from pyspark.sql.functions import when,col
 df2 = df.withColumn("new_gender", when(df.gender == "M","Male")
                                  .when(df.gender == "F","Female")
                                  .when(df.gender.isNull() ,"")
@@ -25,8 +27,8 @@ df2=df.select(col("*"),when(df.gender == "M","Male")
                   .when(df.gender.isNull() ,"")
                   .otherwise(df.gender).alias("new_gender"))
 df2.show()
+
 # Using SQL Case When
-from pyspark.sql.functions import expr
 df3 = df.withColumn("new_gender", expr("CASE WHEN gender = 'M' THEN 'Male' " + 
            "WHEN gender = 'F' THEN 'Female' WHEN gender IS NULL THEN ''" +
           "ELSE gender END"))
