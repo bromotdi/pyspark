f7e03b90e8fff00863600db56ea54626c4604f83
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-18 21:51:39 +0200

    Update pyspark-window-functions.py

--- sql-functions/pyspark-window-functions.py
+++ sql-functions/pyspark-window-functions.py
@@ -1,5 +1,15 @@
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.window import Window
+from pyspark.sql.functions import row_number
+from pyspark.sql.functions import rank
+from pyspark.sql.functions import dense_rank
+from pyspark.sql.functions import percent_rank    
+from pyspark.sql.functions import ntile
+from pyspark.sql.functions import cume_dist    
+from pyspark.sql.functions import lag 
+from pyspark.sql.functions import lead  
+from pyspark.sql.functions import col,avg,sum,min,max,row_number 
 
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 
@@ -22,43 +32,35 @@ df = spark.createDataFrame(data = simpleData, schema = columns)
 df.printSchema()
 df.show(truncate=False)
 
-from pyspark.sql.window import Window
-from pyspark.sql.functions import row_number
+
 windowSpec  = Window.partitionBy("department").orderBy("salary")
 
 df.withColumn("row_number",row_number().over(windowSpec)) \
     .show(truncate=False)
 
-from pyspark.sql.functions import rank
 df.withColumn("rank",rank().over(windowSpec)) \
     .show()
 
-from pyspark.sql.functions import dense_rank
 df.withColumn("dense_rank",dense_rank().over(windowSpec)) \
     .show()
 
-from pyspark.sql.functions import percent_rank
 df.withColumn("percent_rank",percent_rank().over(windowSpec)) \
     .show()
-    
-from pyspark.sql.functions import ntile
+
 df.withColumn("ntile",ntile(2).over(windowSpec)) \
     .show()
 
-from pyspark.sql.functions import cume_dist    
 df.withColumn("cume_dist",cume_dist().over(windowSpec)) \
    .show()
-
-from pyspark.sql.functions import lag    
+   
 df.withColumn("lag",lag("salary",2).over(windowSpec)) \
       .show()
-
-from pyspark.sql.functions import lead    
+  
 df.withColumn("lead",lead("salary",2).over(windowSpec)) \
     .show()
     
 windowSpecAgg  = Window.partitionBy("department")
-from pyspark.sql.functions import col,avg,sum,min,max,row_number 
+
 df.withColumn("row",row_number().over(windowSpec)) \
   .withColumn("avg", avg(col("salary")).over(windowSpecAgg)) \
   .withColumn("sum", sum(col("salary")).over(windowSpecAgg)) \
