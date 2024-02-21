8962661fa6a5089f6becf926f218c70b6eecd3de
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-21 23:54:54 +0200

    Update pyspark-collect.py

--- dataframe-operations/pyspark-collect.py
+++ dataframe-operations/pyspark-collect.py
@@ -8,6 +8,7 @@ dept = [("Finance",10), \
     ("Sales",30), \
     ("IT",40) \
   ]
+
 deptColumns = ["dept_name","dept_id"]
 deptDF = spark.createDataFrame(data=dept, schema = deptColumns)
 deptDF.printSchema()
@@ -22,4 +23,3 @@ print(dataCollect2)
 
 for row in dataCollect:
     print(row['dept_name'] + "," +str(row['dept_id']))
-
