03419c502c6f6e6d90890b659c966e0d04ce7452
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-21 23:54:29 +0200

    Update pyspark-change-string-double.py

--- dataframe-operations/pyspark-change-string-double.py
+++ dataframe-operations/pyspark-change-string-double.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import DoubleType, IntegerType
+from pyspark.sql.functions import col, round, expr
+
 # Create SparkSession
 spark = SparkSession.builder \
           .appName('SparkByExamples.com') \
@@ -15,16 +17,14 @@ df = spark.createDataFrame(data = simpleData, schema = columns)
 df.printSchema()
 df.show(truncate=False)
 
-from pyspark.sql.functions import col,round,expr
 df.withColumn("salary",df.salary.cast('double')).printSchema()    
 df.withColumn("salary",df.salary.cast(DoublerType())).printSchema()    
 df.withColumn("salary",col("salary").cast('double')).printSchema()    
 
-#df.withColumn("salary",round(df.salary.cast(DoubleType()),2)).show(truncate=False).printSchema()    
+df.withColumn("salary",round(df.salary.cast(DoubleType()),2)).show(truncate=False).printSchema()    
 df.selectExpr("firstname","isGraduated","cast(salary as double) salary").printSchema()    
 
 df.createOrReplaceTempView("CastExample")
 spark.sql("SELECT firstname,isGraduated,DOUBLE(salary) as salary from CastExample").printSchema()
 
-
-#df.select("firstname",expr(df.age),"isGraduated",col("salary").cast('float').alias("salary")).show()
+df.select("firstname",expr(df.age),"isGraduated",col("salary").cast('float').alias("salary")).show()
