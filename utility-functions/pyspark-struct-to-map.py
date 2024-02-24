423a0ab92d2f32a22219502a59c4285380bbce81
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-24 22:47:02 +0200

    Update pyspark-struct-to-map.py

--- utility-functions/pyspark-struct-to-map.py
+++ utility-functions/pyspark-struct-to-map.py
@@ -1,12 +1,15 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql.functions import col,lit,create_map
 
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
 data = [ ("36636","Finance",(3000,"USA")), 
     ("40288","Finance",(5000,"IND")), 
     ("42114","Sales",(3900,"USA")), 
     ("39192","Marketing",(2500,"CAN")), 
     ("34534","Sales",(6500,"USA")) ]
+
 schema = StructType([
      StructField('id', StringType(), True),
      StructField('dept', StringType(), True),
@@ -20,9 +23,7 @@ df = spark.createDataFrame(data=data,schema=schema)
 df.printSchema()
 df.show(truncate=False)
 
-
 #Convert struct type to Map
-from pyspark.sql.functions import col,lit,create_map
 df = df.withColumn("propertiesMap",create_map(
         lit("salary"),col("properties.salary"),
         lit("location"),col("properties.location")
