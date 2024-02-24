129062378b28d05ba441d7c0b2c15ab6805a09b0
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-24 22:44:51 +0200

    Update pyspark-lit.py

--- utility-functions/pyspark-lit.py
+++ utility-functions/pyspark-lit.py
@@ -6,6 +6,7 @@ from pyspark.sql.functions import when
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 data = [("111",50000),("222",60000),("333",40000)]
 columns= ["EmpId","Salary"]
+
 df = spark.createDataFrame(data = data, schema = columns)
 df.printSchema()
 df.show(truncate=False)
