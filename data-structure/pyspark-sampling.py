785fe4a8ebfeb71a60d28a86ebf651f98334f73a
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-25 21:59:34 +0200

    Update pyspark-sampling.py

--- data-structure/pyspark-sampling.py
+++ data-structure/pyspark-sampling.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+
 spark = SparkSession.builder \
     .master("local[1]") \
     .appName("SparkByExamples.com") \
@@ -16,12 +17,9 @@ print(df.sample(True,0.3,123).collect())
 print(df.sample(0.3,123).collect())
 
 '''sampleBy() '''
-print("sampleBy Examples")
 df2=df.select((df.id % 3).alias("key"))
 print(df2.sampleBy("key", {0: 0.1, 1: 0.2},0).collect())
 
-
-print("RDD Examples")
 '''RDD'''
 rdd = spark.sparkContext.range(0,100)
 print(rdd.sample(False,0.1,0).collect())
