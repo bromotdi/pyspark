f9e985a2876ebdf75b7bbe04444c1e66123e1aab
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-25 22:03:55 +0200

    Update pyspark-show-top-n-rows.py

--- data-structure/pyspark-show-top-n-rows.py
+++ data-structure/pyspark-show-top-n-rows.py
@@ -6,12 +6,10 @@ spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 simpleData = [("James",34),("Ann",34),
     ("Michael",33),("Scott",53),
     ("Robert",37),("Chad",27)
-  ]
+]
 
 columns = ["firstname","age",]
 df = spark.createDataFrame(data = simpleData, schema = columns)
-
-
 df.show()
 
 #Returns the first ``num`` rows as a :class:`list` of :class:`Row`.
@@ -26,16 +24,13 @@ print(df.take(2))
 print(df.tail(2))
 
 """Returns the first ``n`` rows.
-
 .. note:: This method should only be used if the resulting array is expected
     to be small, as all the data is loaded into the driver's memory.
-
 :param n: int, default 1. Number of rows to return.
 :return: If n is greater than 1, return a list of :class:`Row`.
     If n is 1, return a single Row."""
-#Return Array[T]
-print(df.head(2))
 
+print(df.head(2))
 
 #Returns the first row, same as df.head(1)
 print(df.first())
@@ -44,7 +39,7 @@ print(df.first())
 #Action, Return Array[T]
 print(df.collect())
 
-#"Limits the result count to the number specified.
+#Limits the result count to the number specified.
 #Returns a new Dataset by taking the first n rows.
 pandasDF=df.limit(3).toPandas()
 print(pandasDF)
